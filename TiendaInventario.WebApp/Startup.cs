11720628d4ecbb46de930b644e401c6910705e58
carlosclavijo <carlo96cf@gmail.com>
2020-12-15 23:57:40 -0400

    Permiso Cors añadido

--- TiendaInventario.WebApp/Startup.cs
+++ TiendaInventario.WebApp/Startup.cs
@@ -32,6 +32,11 @@ namespace TiendaInventario.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => {
+                options.AddDefaultPolicy(builder => {
+                    builder.WithOrigins("http://localhost:4200");
+                });
+            });
             services.AddApplication();
             services.AddInfrastructure(Configuration);
             services.AddControllers();
@@ -62,7 +67,7 @@ namespace TiendaInventario.WebApp
 
             app.UseSerilogRequestLogging();
             app.UseRouting();
-
+            app.UseCors();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
